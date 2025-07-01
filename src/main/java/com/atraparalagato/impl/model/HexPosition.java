2bad602402a35648d561cf8a000ba04e6fd34fb5
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-06-30 22:29:40 -0400

    Update HexPosition.java

--- src/main/java/com/atraparalagato/impl/model/HexPosition.java
+++ src/main/java/com/atraparalagato/impl/model/HexPosition.java
@@ -1,10 +1,9 @@
+// HexPosition.java
 package com.atraparalagato.impl.model;
 
 import com.atraparalagato.base.model.Position;
 
-import java.util.Objects;
-
-public class HexPosition implements Position {
+public class HexPosition extends Position {
     private final int q;
     private final int r;
 
@@ -22,26 +21,55 @@ public class HexPosition implements Position {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof HexPosition)) return false;
-        HexPosition that = (HexPosition) o;
-        return q == that.q && r == that.r;
+    public double distanceTo(Position other) {
+        if (!(other instanceof HexPosition)) return Double.MAX_VALUE;
+        HexPosition o = (HexPosition) other;
+        int dq = Math.abs(q - o.q);
+        int dr = Math.abs(r - o.r);
+        int ds = Math.abs((-q - r) - (-o.q - o.r));
+        return (dq + dr + ds) / 2.0;
+    }
+
+    @Override
+    public Position add(Position other) {
+        if (!(other instanceof HexPosition)) return this;
+        HexPosition o = (HexPosition) other;
+        return new HexPosition(q + o.q, r + o.r);
+    }
+
+    @Override
+    public Position subtract(Position other) {
+        if (!(other instanceof HexPosition)) return this;
+        HexPosition o = (HexPosition) other;
+        return new HexPosition(q - o.q, r - o.r);
+    }
+
+    @Override
+    public boolean isAdjacentTo(Position other) {
+        return distanceTo(other) == 1.0;
+    }
+
+    @Override
+    public boolean isWithinBounds(int maxSize) {
+        int s = -q - r;
+        return Math.abs(q) <= maxSize && Math.abs(r) <= maxSize && Math.abs(s) <= maxSize;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(q, r);
+        return 31 * q + r;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        HexPosition that = (HexPosition) obj;
+        return q == that.q && r == that.r;
     }
 
     @Override
     public String toString() {
-        return "HexPosition(" + q + "," + r + ")";
-    }
-
-    public double distanceTo(HexPosition other) {
-        return (Math.abs(q - other.q)
-                + Math.abs(q + r - other.q - other.r)
-                + Math.abs(r - other.r)) / 2.0;
+        return "(" + q + ", " + r + ")";
     }
 }
