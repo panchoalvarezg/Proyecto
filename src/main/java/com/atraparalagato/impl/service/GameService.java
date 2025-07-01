fa6d8e040a841d9eabc1920692d04f6a1dcf1534
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-07-01 05:44:55 -0400

    Update GameService.java

--- src/main/java/com/atraparalagato/impl/service/GameService.java
+++ src/main/java/com/atraparalagato/impl/service/GameService.java
@@ -5,6 +5,8 @@ import com.atraparalagato.impl.repository.GameScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GameService {
 
@@ -15,12 +17,17 @@ public class GameService {
         this.gameScoreRepository = gameScoreRepository;
     }
 
-    // Ahora recibe el gameId y lo guarda
+    // Guarda el puntaje
     public void saveScore(String gameId, String playerName, Integer score) {
         GameScore gameScore = new GameScore();
         gameScore.setGameId(gameId);
         gameScore.setPlayerName(playerName);
         gameScore.setScore(score);
         gameScoreRepository.save(gameScore);
+    }
+
+    // Devuelve todos los puntajes guardados
+    public List<GameScore> getAllScores() {
+        return gameScoreRepository.findAll();
     }
 }
