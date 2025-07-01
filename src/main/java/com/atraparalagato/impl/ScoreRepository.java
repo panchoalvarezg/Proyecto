51ee7c50bc768f22bd6889fb511ab48eafd4d6a1
panchoalvarezg <franciscoialvarez@alumnos.uai.cl>
2025-07-01 05:00:34 -0400

    Update and rename ScoreRepository.java to GameScoreRepository.java

--- src/main/java/com/atraparalagato/impl/ScoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.proyecto.impl;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-    List<Score> findTop10ByOrderByPlayerWonDescMovesCountAsc();
-}
